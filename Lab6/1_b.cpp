61a87c56a98c9b748e23e02ce2825bf6c2d6c9f5
Anantha Sai Satwik Vysyaraju <saisatwik2226@gmail.com>
2021-10-09 11:23:54 +0530

    working

--- Lab6/1_b.cpp
+++ Lab6/1_b.cpp
@@ -9,9 +9,9 @@
 using namespace std;
 
 int binarySearchIterative(int* arr, int len, int ele){
-    int low = 0, high = len;
+    int low = 0, high = len-1;
     int mid1,mid2;
-    while (low<high)
+    while (high-low>3)
     {
         mid1 = (low+high)/3;
         mid2 = 2*(low+high)/3;
@@ -19,66 +19,63 @@ int binarySearchIterative(int* arr, int len, int ele){
         {
             high = mid1;
         }
-        else if (arr[mid2]>=ele)
+        if (arr[mid2]>=ele && ele>arr[mid1])
         {
             high = mid2;
+            low = mid1+1;
         }
-
         if(arr[mid2]<ele){
             low = mid2+1;
-        }
-        else if(arr[mid1]<ele)
-        {
-            low = mid1+1;
-        }              
-        cout<<low<<" "<<high<<endl;
+        }             
+        // cout<<low<<" "<<high<<endl;
     }
-    if(low<len && arr[low]<ele)
+    if(arr[low]==ele)
 		return low+1;
-	return low;
+    else if(arr[low+1]==ele)
+        return low+2;
+    else if(arr[high]==ele)
+        return high;
+	return -1;
 }
 
 int binarySearchRecursive(int* arr, int len, int ele, int low, int high){
 	//base case
-	if(low>=high){
-		if(low<len && arr[low]<ele){
-			return low+1;
-		}
-		else {
-			return low;
-		}
+	if(high-low>3){
+		if(arr[low]==ele)
+		return low+1;
+        else if(arr[low+1]==ele)
+            return low+2;
+        else if(arr[high]==ele)
+            return high;
+        return -1;
 	}
     int mid1 = (low+high)/3;
     int mid2 = 2*(low+high)/3;
-	if (arr[mid1]>=ele)
+    if (arr[mid1]>=ele)
     {
         high = mid1;
     }
-    else if (arr[mid2]>ele)
+    if (arr[mid2]>=ele && ele>arr[mid1])
     {
         high = mid2;
+        low = mid1+1;
     }
-
     if(arr[mid2]<ele){
         low = mid2+1;
     }
-    else if(arr[mid1]<ele)
-    {
-        low = mid1+1;
-    }      
-    
 	return binarySearchRecursive(arr, len, ele, low, high);
 }
 int main(){
 	int n;
-	//cin>>n;
     n=10;
-	int arr[n] = {1,2,3,4,5,6,7,8,9,10};
-	// for(int i=0; i<n; i++){
-	// 	cin>>arr[i];
-	// }
-	int element=9;
+	int arr[n] = {1,2,3,4,5,6,8,9,10};
+	int element=7;
 	// cin>>element;
-	cout<<"Index using iterative solution: "<<binarySearchIterative(arr, n, element)<<endl;
-	// cout<<"Index using recursive solution: "<<binarySearchRecursive(arr, n, element, 0, n)<<endl;
+    int x = binarySearchIterative(arr, n, element);
+    if (x>0)
+	cout<<"Index using iterative solution: "<<x<<endl;
+    else
+    cout<<"Not Found"<<endl;
+    
+	cout<<"Index using recursive solution: "<<binarySearchRecursive(arr, n, element, 0, n)<<endl;
 }
